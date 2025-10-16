75df9145640bcb6a1ffaceadbaed86c6363c40e2
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2025-10-15 22:09:34 -0400

    feat(async_node): The async node is more or less done now

--- src/core/async_impl/async_node.rs
+++ src/core/async_impl/async_node.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use crate::core::Executable;
 use crate::sync::node::Node;
 use crate::sync_impl::AsAny;
 use crate::sync_impl::NodeValue;
@@ -13,21 +14,30 @@ pub struct AsyncNode {
     pub behaviour: Box<dyn AsyncNodeLogic>,
 }
 
+impl Clone for AsyncNode {
+    fn clone(&self) -> Self {
+        Self {
+            data: self.data.clone(), 
+            behaviour: self.behaviour.clone_box(),
+        }
+    }
+}
+
 impl AsyncNode {
     pub fn new<L: AsyncNodeLogic>(behaviour: L) -> Self {
         AsyncNode {
             data: NodeCore::default(),
-            behaviour,
+            behaviour: Box::new(behaviour),
         }
     }
 
     pub fn set_params(&mut self, params: HashMap<String, NodeValue>) {
         self.data.params = params;
     }
-    pub fn next(self, node: Node) -> Self {
+    pub fn next(self, node: Executable) -> Self {
         self.next_on(node, "default")
     }
-    pub fn next_on(mut self, node: Node, action: &str) -> Self {
+    pub fn next_on(mut self, node: Executable, action: &str) -> Self {
         if self.data.successors.contains_key(action) {
             log::warn!(
                 "Warning: Action {} was found in successors, Overwriting key {}.",
@@ -38,6 +48,23 @@ impl AsyncNode {
         self.data.successors.insert(action.to_string(), node);
         self
     }
+
+
+    pub async fn run(&self, shared: &mut HashMap<String, NodeValue>) -> Option<String> {
+        let p = self.behaviour.prep(&self.data.params, shared).await;
+        let e = self.behaviour.exec(p.clone()).await;
+        self.behaviour.post(shared, p, e).await
+    }
+
+    pub async fn run_with_params(
+        &self,
+        shared: &mut HashMap<String, NodeValue>,
+        param: &HashMap<String, NodeValue>,
+    ) -> Option<String> {
+        let p = self.behaviour.prep_async(param, shared).await;
+        let e = self.behaviour.exec_async(p.clone()).await;
+        self.behaviour.post_async(shared, p, e).await
+    }
 }
 
 // More or less the same logic as NodeLogic
@@ -46,13 +73,13 @@ pub trait AsyncNodeLogic: AsAny + Send + Sync + 'static {
     // Required method
     fn clone_box(&self) -> Box<dyn AsyncNodeLogic>;
 
-    async fn prep_async(
+    async fn prep(
         &self,
         _params: &HashMap<String, NodeValue>,
         _shared: &HashMap<String, NodeValue>,
     ) -> NodeValue;
-    async fn exec_async(&self, _input: NodeValue) -> NodeValue;
-    async fn post_async(
+    async fn exec(&self, _input: NodeValue) -> NodeValue;
+    async fn post(
         &self,
         _shared: &mut HashMap<String, NodeValue>,
         _prep_res: NodeValue,
